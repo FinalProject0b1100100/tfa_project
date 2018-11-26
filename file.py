5067c9f2e47999938db5a48e3db358e00bd3a04a
ql2333 <ql2333@columbia.edu>
2018-11-26 19:31:58 +0000

    version 3 apply get_lat_log and get_address functions to data sample, write a get_map() function to get a map of the result

--- file.py
+++ file.py
@@ -1,7 +1,7 @@
 import requests
 # try to find the latitude and longitude of a place
 # refer to couese material in Data Analytics
-api_key = 'AIzaSyAcJo9m6XPc5L32vRt6BXTfeXdVxw81n78')
+# api_key can't be posed in github
 # create a function to get the latitude and longitude of a place
 def get_location_data(address):
     response_data = ''
@@ -22,13 +22,26 @@ def get_lat_lng(address_string,api_key):
     response_data = get_location_data(address_string+'New York')
     return (response_data['results'][0]['geometry']['location']['lat'],
            response_data['results'][0]['geometry']['location']['lng'])
+# example: get_lat_lng("Columbia University",api_key)
 # create a function to get the address of a place in details
 def get_address(address_string,api_key):
     response_data = get_location_data(address_string+'New York')
     return response_data['results'][0]['formatted_address']
-get_address("Columbia University",api_key)
-# create imaginative sample data (since the data from web crawler part is not accessible now)
+# example: get_address("Columbia University",api_key)
+# create imaginary sample data (since the data from web crawler part is not accessible now)
 import pandas as pd
 df = pd.DataFrame({'attraction':['Columbia University','Manhattan Skyline'],'price':[1,2]})
-  
+# apply former function into data sample
+df['lat'],df['lng'] = df.apply(lambda x: get_lat_lng(x['attraction'],api_key),axis=1)
+df['address'] = df.apply(lambda x: get_address(x['attraction'],api_key),axis=1)
+# create a function to get a map of the result
+import folium
+def get_map(startpoint,result):
+    startpoint = get_lat_lng(startpoint,api_key)
+    m = folium.Map(location=startpoint,zoom_start=14)
+    for i in range(len(df)):
+        folium.Marker([df.iloc[i]['lat'],df.iloc[i]['lng']],popup=df.iloc[i]['attraction']).add_to(m)
+    return m
+# exmple : get_map('Mona museum',df)
+
 
