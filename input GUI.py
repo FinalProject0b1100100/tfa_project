43542774245fc8b14381c2c02e3f911abf87d59b
Weiyi <wh2422@columbia.edu>
2018-11-29 20:19:03 -0500

    realize basic fetch function
    allow input to be store in local variable, need to change the window design for better user experience

--- input GUI.py
+++ input GUI.py
@@ -24,25 +24,41 @@ class App(object):
         self.phb_entry.grid(row=self.current_row, column=1)
         self.phb_entry.menu  =  Menu ( self.phb_entry, tearoff = 0 )
         self.phb_entry["menu"]  =  self.phb_entry.menu
-#       Item0 = IntVar()
+        #Item0 = IntVar()
+        #Item1 = IntVar()
+        #Item2 = IntVar()
+        ptg_list=[]
+        for x in range(3):
+            x=IntVar()
+            ptg_list.append(x)
+        for x in range(3):
+            self.phb_entry.menu.add_checkbutton ( label="Item"+str(x),variable=ptg_list[x])
+        self.current_row+=1    
         #####----------change this-----------####
-        self.phb_entry.menu.add_checkbutton ( label="Item0", variable=Item0)
-        self.phb_entry.menu.add_checkbutton ( label="Item1", variable=Item1)
-        self.phb_entry.menu.add_checkbutton ( label="Item2", variable=Item2)
-        self.current_row+=1
+        #self.phb_entry.menu.add_checkbutton ( label="Item0", variable=Item0)
+        #self.phb_entry.menu.add_checkbutton ( label="Item1", variable=Item1)
+        #self.phb_entry.menu.add_checkbutton ( label="Item2", variable=Item2)
+        #self.current_row+=1
         
         #place_to_go
         self.ptg_label=tk.Label(window,text="Places that you want to go:")
         self.ptg_label.grid(row=self.current_row, column=0)
-        self.ptg_entry=  Menubutton (window, text="Please select", relief=RAISED )
+        self.ptg_entry=  Menubutton (window, text="Please select all", relief=RAISED )
         self.ptg_entry.grid(row=self.current_row, column=1)
         self.ptg_entry.menu  =  Menu ( self.ptg_entry, tearoff = 0 )
         self.ptg_entry["menu"]  =  self.ptg_entry.menu
+        phb_list=[]
+        for x in range(3):
+            x=IntVar()
+            phb_list.append(x)
+        for x in range(3):
+            self.ptg_entry.menu.add_checkbutton ( label="Item"+str(x),variable=phb_list[x])
+        self.current_row+=1  
         #####----------change this-----------####
-        self.ptg_entry.menu.add_checkbutton ( label="Item0", variable=Item0)
-        self.ptg_entry.menu.add_checkbutton ( label="Item1", variable=Item1)
-        self.ptg_entry.menu.add_checkbutton ( label="Item2", variable=Item2)
-        self.current_row+=1
+        #self.ptg_entry.menu.add_checkbutton ( label="Item0", variable=Item0)
+        #self.ptg_entry.menu.add_checkbutton ( label="Item1", variable=Item1)
+        #self.ptg_entry.menu.add_checkbutton ( label="Item2", variable=Item2)
+        #self.current_row+=1
 
         #duration
         self.duration_label=tk.Label(window,text="Your everyday travel duration is:")
@@ -71,28 +87,41 @@ class App(object):
         self.sort_entry=OptionMenu(window, self.sort_text, *list_)
         self.sort_entry.grid(row=self.current_row, column=1)
         self.current_row+=1
+        self.current_row+=1
         
 
         #OK button
         self.query_button=tk.Button(window, text="OK")
         self.query_button.configure(command=self.fetch)
         self.query_button.grid(row=self.current_row, column=0, columnspan=1)
-        #self.current_row+=1
+        
 
         #quit button
         self.quit_button=tk.Button(window, text="Quit")
         self.quit_button.configure(command=quit)
         self.quit_button.grid(row=self.current_row, column=1, columnspan=1)
         self.current_row+=1
-    
+
     def fetch(self):
-        
         #create local variable
-        fname=self.fname_text.get()
-        lname=self.lname_text.get()
-        age=self.age_text.get()
-        sex=self.sex_text.get()
-        income=self.income_text.get()
+        home=self.home_text.get()
+        duration=self.duration_text.get()
+        place_have_been=[]
+        for x in range(3):
+            if x==1:
+                place_have_been.append(x)
+        #place_to_go=self.sex_text.get()
+        citypass=self.cp_text.get()
+        sort=self.sort_text.get()
+
+        print(home)
+        print(duration)
+        for x in range(len(place_have_been)):
+            print (place_have_been[x])
+        #print(place_to_go)
+        print(citypass)
+        print(sort)
+        
 
 
             
