10d35ab54338cdaa6cfa4d492d8d6617ffa90bbc
Luokunren <44420488+Luokunren@users.noreply.github.com>
2018-12-01 06:18:51 +0800

    Add price and priority

--- Recommandation by Distance.py
+++ Recommandation by Distance.py
@@ -1,12 +1,13 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[197]:
 
 
 df = pd.DataFrame({'attraction':['Columbia University','Manhattan Skyline','Time Square','Central Park','Soho'],'duration':[1,2,3,4,5],'rank':[4.3,4.5,3.9,4.4,4.8],'type':['University','Skyscraper','Square','Park','Shopping']})
 import requests
 api_key = 'AIzaSyAcJo9m6XPc5L32vRt6BXTfeXdVxw81n78'
+# get_location
 def get_location_data(address):
     response_data = ''
     url="https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (address,api_key)
@@ -22,11 +23,11 @@ def get_location_data(address):
     except:
         print("Something went wrong with requests.get")
     return response_data
+# get _lat_lng
 def get_lat_lng(address_string,api_key):
     response_data = get_location_data(address_string+'New York')
     return (response_data['results'][0]['geometry']['location']['lat'],
            response_data['results'][0]['geometry']['location']['lng'])
-
 df['location'] = df.apply(lambda x: get_lat_lng(x['attraction'],api_key),axis=1)
 startpoint = 'museum of modern art'
 startpoint_ll =  get_lat_lng(startpoint,api_key)
@@ -39,8 +40,7 @@ priority = 'distance'
 import math
 import pandas as pd
 
-
-def recommendation(df, startpoint, visited, preference, duration = 24, price):
+def recommendation(df, startpoint, visited, preference, priority, duration = 24,price):
     #startpoint is a string usually recording the hotel's location
     #visited is a string recording where you have been 
     #preference is a string representing the type of attractions you want to visited
@@ -55,6 +55,7 @@ def recommendation(df, startpoint, visited, preference, duration = 24, price):
     df_1 = df#df_1 now is the dataframe without rows whose name is in visited list
     
     #prefrence is the first criterion
+    
     df_2 = df_1[df_1['type'].isin(preference)]#df_2 now is the dataframe only with rows whose 'type' is in preference list 
     
     #distance is the second criterion   
@@ -66,16 +67,33 @@ def recommendation(df, startpoint, visited, preference, duration = 24, price):
     def get_distance(x):
         return math.sqrt(pow(x[0] - startpoint_x, 2)+ pow(x[1]- startpoint_y, 2))
     df_2['distance'] = df_2.apply(lambda x: get_distance(x['location']),axis = 1)
-    df_3 = df_2.sort_values(by='distance') # df_3 now is the sorted dataframe by 'distance'
+    
+    # When sorting by distance, ascending. When sorting by rank, descending
+    df_3 = df_2.sort_values(by=priority,ascending=(priority=='distance')) # df_3 now is the sorted dataframe by priority('distance' or 'rank')
     
     #calculate time duration to derive the number of places to visit
     n = 0
     sum_time = 0
+
+    #extract the name of sorted attractions
     while sum_time < duration & n < len(df_3):
         time = df_3.iloc[n]['duration']#extract time of the corresponding attractions
         sum_time =+ time
         n += 1
     
-    result = df_3.iloc[:n-1]
+    df_4 = df_3.iloc[:n-1]
+    
+    #calculate total money to derive the number of places to visit
+    n = 0
+    total_price = 0
+    
+    #extract the name of sorted attractions
+    while total_price < money & n < len(df_4):
+        time = df_3.iloc[n]['price']#extract time of the corresponding attractions
+        sum_money =+ money
+        n += 1
+        
+    #extract the top attractions by distance within time limit
+    result = df_4.iloc[:n-1]
     return result
 
