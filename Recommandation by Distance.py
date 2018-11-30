a550a9ca3de6cce175215815fac9414d0400ef0e
Luokunren <44420488+Luokunren@users.noreply.github.com>
2018-12-01 07:54:17 +0800

    Use the real dataframe

--- Recommandation by Distance.py
+++ Recommandation by Distance.py
@@ -1,11 +1,65 @@
 
 # coding: utf-8
 
-# In[201]:
+# In[240]:
 
 
-#init dataframe
-df = pd.DataFrame({'attraction':['Columbia University','Manhattan Skyline','Time Square','Central Park','Soho'],'duration':[1,2,3,4,5],'rank':[4.3,4.5,3.9,4.4,4.8],'type':['University','Skyscraper','Square','Park','Shopping']})
+import pandas as pd
+df = pd.read_csv("data_with_location.csv", index_col = 0)
+
+
+# In[241]:
+
+
+df.head()
+
+
+# In[242]:
+
+
+df = df.rename(index=str, columns={"attraction_name": "attraction", "attraction_type": "type", "attraction_rank": "rank", "attraction_duration": "duration", "attraction_lat": "lat", "attraction_lng": "lng"})
+
+
+# In[243]:
+
+
+loc = list()
+for i in range(len(df)):
+    a = df['lat'][i]
+    b = df['lng'][i]
+    loc.append((a, b))
+df['location'] = loc
+
+
+# In[244]:
+
+
+df.head()
+
+
+# In[254]:
+
+
+# import all the libraries
+import pandas as pd
+import math
+
+df = pd.read_csv("data_with_location.csv", index_col = 0)
+# check the dataframe
+# df.head()
+
+df = df.rename(index=str, columns={"attraction_name": "attraction", "attraction_type": "type", "attraction_rank": "rank", "attraction_duration": "duration", "attraction_lat": "lat", "attraction_lng": "lng"})
+
+# combine lat and lng as 'location' and add to the dataframe
+loc = list()
+for i in range(len(df)):
+    a = df['lat'][i]
+    b = df['lng'][i]
+    loc.append((a, b))
+df['location'] = loc
+
+# check the dataframe
+# df.head()
 
 # use google API to transform the locaiton into a string, which represents the longitude and latitude
 import requests
@@ -28,30 +82,19 @@ def get_location_data(address):
         print("Something went wrong with requests.get")
     return response_data
 
-# get _lat_lng
-def get_lat_lng(address_string,api_key):
-    response_data = get_location_data(address_string+'New York')
-    return (response_data['results'][0]['geometry']['location']['lat'],
-           response_data['results'][0]['geometry']['location']['lng'])
-df['location'] = df.apply(lambda x: get_lat_lng(x['attraction'],api_key),axis=1)
-
-#these are continued to be substituted
+# these are continued to be substituted
 startpoint = 'museum of modern art'
-startpoint_ll =  get_lat_lng(startpoint,api_key)
-visited = ['Columbia University']
-preference = ['Shopping','Park','Square']
+startpoint_location =  get_lat_lng(startpoint,api_key)
+visited = ['Central Park']
+preference = ['Park','Museum']
 duration = 9
 priority = 'distance'
 
-
-import math
-import pandas as pd
-
-
 def recommendation(df, startpoint, visited, preference, priority, duration = 24):
-    #visited is a string recording the visited places 
-    #preference is a string representing the preference
-    #priority is ordering by rank or distance
+    #startpoint is a string usually recording the hotel's longtitude and latitude
+    #visited is a string recording where you have been
+    #preference is a string representing the type of attractions that you want to visit
+    #priority is crorder by rank or distance
     #duration is the upper time limit of all selected attractions
     
     #exclude attractions that visitors has been before    
@@ -65,12 +108,12 @@ def recommendation(df, startpoint, visited, preference, priority, duration = 24)
     df_2 = df_1[df_1['type'].isin(preference)]#df_2 now is the dataframe only with rows whose 'type' is in preference list
       
     #get the x & y of startpoint
-    startpoint_x = startpoint[0]
-    startpoint_y = startpoint[1]
+    startpoint_x = startpoint_location[0]
+    startpoint_y = startpoint_location[1]
 
     #add the distance between startpoint and attractions as 'distance' to df_2
-    def get_distance(x):
-        return math.sqrt(pow(x[0] - startpoint_x, 2)+ pow(x[1]- startpoint_y, 2))
+    def get_distance(location):
+        return math.sqrt(pow(location[0] - startpoint_x, 2)+ pow(location[1]- startpoint_y, 2))
     df_2['distance'] = df_2.apply(lambda x: get_distance(x['location']),axis = 1)
     
     #When sorting by distance, ascending. When sorting by rank, descending.
@@ -87,7 +130,7 @@ def recommendation(df, startpoint, visited, preference, priority, duration = 24)
         n += 1
     
     result = df_3.iloc[:n-1]
+    result = result.reset_index(drop = True)
     return result
-a = recommendation (df, startpoint_ll, visited, preference, priority, duration)
-a
+# r = recommendation (df, startpoint_location, visited, preference, priority, duration)
 
